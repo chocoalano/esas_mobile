64e3e66b136ddf32b0a2468bdd858467841f2517
chocoalano <alangentina95@gmail.com>
2024-11-13 11:55:56 +0700

    V2

--- lib/app/modules/home/display/views/components/empcard.dart
+++ lib/app/modules/home/display/views/components/empcard.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:esas_mobile/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
